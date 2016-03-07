a13613de32c123c4f3bbb30ecb331a80b21f37e1
wangrl <ruilingwang@163.com>
2016-03-07 16:48:52 +0800

    新的代码提交

--- web/src/main/java/org/smart/h2/DbUtils.java
+++ web/src/main/java/org/smart/h2/DbUtils.java
@@ -1,11 +1,13 @@
 package org.smart.h2;
 
+import org.apache.commons.dbutils.QueryRunner;
+
 /**
  * @author: wangrl
  * @Date: 2016-03-06 22:30
  */
 public class DbUtils {
-    private static final QueryReturn queryReturn = new QueryReturn();
+    private static final QueryRunner queryReturn = new QueryRunner();
 
     public static void main(String[] args) {
         System.out.println(queryReturn);
